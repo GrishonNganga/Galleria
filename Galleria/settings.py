ad511109f2886b0b28adf0f0e6630cb94cdc664c
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-08-05 07:05:29 +0300

    Another heroku conf triasl

--- Galleria/settings.py
+++ Galleria/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
